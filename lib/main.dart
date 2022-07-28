7b71000f0356e4a733b2b99065a7e15603b79f84
0x1772 <realhateux@outlook.com>
2022-07-28 11:56:16 +0300

    mainfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: Text(
+            "Let's Eat",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(child: Image.asset('assets/images/pasta.jpg')),
+          Expanded(child: Image.asset('assets/images/steak.png')),
+        ],
+      ),
     );
   }
 }
